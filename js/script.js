99d066cf61afdcf9e13f275403cf3737e53a6ca5
AlexArro <polsck@gmail.com>
2018-07-06 21:33:26 +0300

    JS: overlay, autofocus

--- js/script.js
+++ js/script.js
@@ -4,23 +4,30 @@ var linkMap = document.querySelector('.map-link');
 var modalMap = document.querySelector('.modal-map');
 var modalFeedbackClose = document.querySelector('.modal-feedback .modal-close-btn');
 var modalMapClose = document.querySelector('.modal-map .modal-close-btn');
+var modalOverlay = document.querySelector('.modal-overlay');
+var modalUserName = modalFeedback.querySelector('[name="fullname"]');
 
 linkFeedback.addEventListener('click', function(evt) {
   evt.preventDefault();
-  modalFeedback.classList.add('modal-show');
+  modalFeedback.classList.add('show');
+  modalOverlay.classList.add('show');
+  modalUserName.focus();
 });
 
 modalFeedbackClose.addEventListener('click', function(evt) {
   evt.preventDefault();
-  modalFeedback.classList.remove('modal-show');
+  modalFeedback.classList.remove('show');
+  modalOverlay.classList.remove('show');
 });
 
 linkMap.addEventListener('click', function(evt) {
   evt.preventDefault();
-  modalMap.classList.add('modal-show');
+  modalMap.classList.add('show');
+  modalOverlay.classList.add('show');
 });
 
 modalMapClose.addEventListener('click', function(evt) {
   evt.preventDefault();
-  modalMap.classList.remove('modal-show');
+  modalMap.classList.remove('show');
+  modalOverlay.classList.remove('show');
 });
