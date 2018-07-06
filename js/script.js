bd61c8bb23e63fc51f2cf737a960d42928d4d96f
AlexArro <polsck@gmail.com>
2018-07-06 18:56:51 +0300

    Popups added

--- js/script.js
+++ js/script.js
@@ -1,4 +1,26 @@
-var linkFeedback = document.querySelector(".feedback-launcher");
-console.log(linkFeedback);
+var linkFeedback = document.querySelector('.feedback-link');
+var modalFeedback = document.querySelector('.modal-feedback');
+var linkMap = document.querySelector('.map-link');
+var modalMap = document.querySelector('.modal-map');
+var modalFeedbackClose = document.querySelector('.modal-feedback .modal-close-btn');
+var modalMapClose = document.querySelector('.modal-map .modal-close-btn');
 
-linkFeedback.addEventListener('click', function() {});
+linkFeedback.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  modalFeedback.classList.add('modal-show');
+});
+
+modalFeedbackClose.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  modalFeedback.classList.remove('modal-show');
+});
+
+linkMap.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  modalMap.classList.add('modal-show');
+});
+
+modalMapClose.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  modalMap.classList.remove('modal-show');
+});
