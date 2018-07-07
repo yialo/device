6d5ed7c6eda584cb86c2ba4b2953451be867cf32
AlexArro <polsck@gmail.com>
2018-07-07 03:20:38 +0300

    New JS features added to index page

--- js/script.js
+++ js/script.js
@@ -1,17 +1,19 @@
 var linkFeedback = document.querySelector('.feedback-link');
 var modalFeedback = document.querySelector('.modal-feedback');
+var modalFullName = modalFeedback.querySelector('.feedback-fullname');
+var modalEmail = modalFeedback.querySelector('.feedback-email');
+var modalForm = modalFeedback.querySelector('.feedback-form');
+var modalFeedbackClose = document.querySelector('.modal-feedback .modal-close-btn');
 var linkMap = document.querySelector('.map-link');
 var modalMap = document.querySelector('.modal-map');
-var modalFeedbackClose = document.querySelector('.modal-feedback .modal-close-btn');
 var modalMapClose = document.querySelector('.modal-map .modal-close-btn');
 var modalOverlay = document.querySelector('.modal-overlay');
-var modalUserName = modalFeedback.querySelector('[name="fullname"]');
 
 linkFeedback.addEventListener('click', function(evt) {
   evt.preventDefault();
   modalFeedback.classList.add('show');
   modalOverlay.classList.add('show');
-  modalUserName.focus();
+  modalFullName.focus();
 });
 
 modalFeedbackClose.addEventListener('click', function(evt) {
@@ -30,4 +32,22 @@ modalMapClose.addEventListener('click', function(evt) {
   evt.preventDefault();
   modalMap.classList.remove('show');
   modalOverlay.classList.remove('show');
+});
+
+modalForm.addEventListener('submit', function(evt) {
+  if (!modalFullName.value || !modalEmail.value) {
+    evt.preventDefault();
+    console.log('Пожалуйста, укажите имя и email');
+  }
+});
+
+window.addEventListener('keydown', function(evt) {
+  if (evt.keyCode === 27) {
+    if (modalOverlay.classList.contains('show')) {
+      evt.preventDefault();
+      modalFeedback.classList.remove('show');
+      modalMap.classList.remove('show');
+      modalOverlay.classList.remove('show');
+    }
+  }
 });
