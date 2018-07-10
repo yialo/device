90d7edaf0a3c9c90736b8552a910559d672ed573
AlexArro <polsck@gmail.com>
2018-07-10 22:32:04 +0300

    window Overlay deleted

--- js/script.js
+++ js/script.js
@@ -8,32 +8,27 @@ var modalFeedbackClose = document.querySelector('.modal-feedback .modal-close-bt
 var linkMap = document.querySelector('.map-link');
 var modalMap = document.querySelector('.modal-map');
 var modalMapClose = document.querySelector('.modal-map .modal-close-btn');
-var modalOverlay = document.querySelector('.modal-overlay');
 
 linkFeedback.addEventListener('click', function(evt) {
   evt.preventDefault();
   modalFeedback.classList.add('modal-show');
-  modalOverlay.classList.add('modal-show');
   modalFullName.focus();
 });
 
 modalFeedbackClose.addEventListener('click', function(evt) {
   evt.preventDefault();
   modalFeedback.classList.remove('modal-show');
-  modalOverlay.classList.remove('modal-show');
   modalFeedback.classList.remove('modal-error');
 });
 
 linkMap.addEventListener('click', function(evt) {
   evt.preventDefault();
   modalMap.classList.add('modal-show');
-  modalOverlay.classList.add('modal-show');
 });
 
 modalMapClose.addEventListener('click', function(evt) {
   evt.preventDefault();
   modalMap.classList.remove('modal-show');
-  modalOverlay.classList.remove('modal-show');
 });
 
 modalForm.addEventListener('submit', function(evt) {
@@ -47,11 +42,10 @@ modalForm.addEventListener('submit', function(evt) {
 
 window.addEventListener('keydown', function(evt) {
   if (evt.keyCode === 27) {
-    if (modalOverlay.classList.contains('modal-show')) {
+    if (modalFeedback.classList.contains('modal-show') || modalMap.classList.contains('modal-show') ) {
       evt.preventDefault();
       modalFeedback.classList.remove('modal-show');
       modalMap.classList.remove('modal-show');
-      modalOverlay.classList.remove('modal-show');
     }
   }
 });
