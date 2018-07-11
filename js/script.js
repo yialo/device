c667ada4ea185fde8882053197e653af5a06f277
AlexArro <polsck@gmail.com>
2018-07-11 21:57:00 +0300

    Map popup fix

--- js/script.js
+++ js/script.js
@@ -5,6 +5,9 @@ var modalEmail = modalFeedback.querySelector('.feedback-email');
 var modalMessage = modalFeedback.querySelector('.feedback-textarea');
 var modalForm = modalFeedback.querySelector('.feedback-form');
 var modalFeedbackClose = document.querySelector('.modal-feedback .modal-close-btn');
+var linkMap = document.querySelector('.map-source');
+var modalMap = document.querySelector('.modal-map');
+var modalMapClose = document.querySelector('.modal-map .modal-close-btn');
 
 linkFeedback.addEventListener('click', function(evt) {
   evt.preventDefault();
@@ -18,6 +21,16 @@ modalFeedbackClose.addEventListener('click', function(evt) {
   modalFeedback.classList.remove('modal-error');
 });
 
+linkMap.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  modalMap.classList.add('modal-show');
+});
+
+modalMapClose.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  modalMap.classList.remove('modal-show');
+});
+
 modalForm.addEventListener('submit', function(evt) {
   if (!modalFullName.value || !modalEmail.value || !modalMessage.value) {
     evt.preventDefault();
@@ -29,9 +42,10 @@ modalForm.addEventListener('submit', function(evt) {
 
 window.addEventListener('keydown', function(evt) {
   if (evt.keyCode === 27) {
-    if (modalFeedback.classList.contains('modal-show')) {
+    if (modalFeedback.classList.contains('modal-show') || modalMap.classList.contains('modal-show') ) {
       evt.preventDefault();
       modalFeedback.classList.remove('modal-show');
+      modalMap.classList.remove('modal-show');
     }
   }
 });
